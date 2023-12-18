61a42223ec57cedb42f88ecd394a3f6cff7731d3
Phillip Schichtel <phillip@schich.tel>
2023-12-18 23:53:46 +0100

    upgrade gradle and kotlin

--- conventions/build.gradle.kts
+++ conventions/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    val kotlinVersion = "1.9.20"
+    val kotlinVersion = "1.9.21"
     implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:$kotlinVersion")
     implementation("org.jetbrains.kotlin.plugin.serialization:org.jetbrains.kotlin.plugin.serialization.gradle.plugin:$kotlinVersion")
 }
