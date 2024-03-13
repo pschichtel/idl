c648ec69b3cda4502051e56947ae64e4ffb876bf
Phillip Schichtel <phillip.schichtel@vier.ai>
2024-03-13 16:27:40 +0100

    upgrade gradle and kotlin

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version("1.9.10") apply(false)
-    kotlin("plugin.serialization") version("1.9.10") apply(false)
+    kotlin("jvm") version("1.9.23") apply(false)
+    kotlin("plugin.serialization") version("1.9.23") apply(false)
     id("tel.schich.idl.plugin") apply(false)
 }
 
